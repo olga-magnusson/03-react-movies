17a060c6b7b7c577a8472c4b6a033ea60591102f
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-02-25 21:06:05 +0100

    change App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,18 +1,18 @@
 import css from './App.module.css';
-import useState from 'react-hot-toast';
-import toast from 'react-hot-toast';
+import useState from 'react';
+import toast, {Toaster} from 'react-hot-toast';
 import SearchBar from '../SearchBar/SearchBar';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
 import { fetchMovies } from '../../services/movieService';
-import { type Movie} from '../../types/movie';
+import type { Movie} from '../../types/movie';
 
 
 
 export default function App() {
-    const [movies, setMovies] = useState<Movie[]>([]);
+    const [movies, setMovies] = useState<Movie[]>( []);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false);
     const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
@@ -40,7 +40,7 @@ export default function App() {
 
       return (
         <div className={css.app}>
-          <toast position="top-right" />
+          <Toaster position="top-right" />
     
           <SearchBar onSubmit={handleSearch} />
     
